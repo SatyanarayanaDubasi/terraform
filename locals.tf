37eec040f9543a7e8074d3b418ab99c3f098d5a5
SatyanarayanaDubasi <satya.dubasi91@gmail.com>
2026-04-28 22:12:16 +0530

    locals variables created

---
+++ locals.tf
@@ -0,0 +1,4 @@
+locals {
+  resource_location = "West Europe"
+  subnet_prefix_id  = ["10.0.0.0/24", "10.0.1.0/24"]
+}
--- main.tf
+++ main.tf
@@ -2,21 +2,21 @@
 
 resource "azurerm_resource_group" "testrg" {
   name     = "testrg"
-  location = "West Europe"
+  location = local.resource_location
 }
 resource "azurerm_virtual_network" "testvnet" {
   name                = "testvnet"
-  location            = "West Europe"
-  resource_group_name = "testrg"
+  location            = local.resource_location
+  resource_group_name = azurerm_resource_group.testrg.name
   address_space       = ["10.0.0.0/16"]
 
   subnet {
     name             = "websubnet"
-    address_prefixes = ["10.0.1.0/24"]
+    address_prefixes = [local.subnet_prefix_id[0]]
   }
 
   subnet {
     name             = "appsubnet"
-    address_prefixes = ["10.0.2.0/24"]
+    address_prefixes = [local.subnet_prefix_id[1]]
   }
 }
