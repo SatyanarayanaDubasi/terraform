f5bd5d3f7d6561fd3d9fd5e675035790703192c8
SatyanarayanaDubasi <satya.dubasi91@gmail.com>
2026-04-29 12:57:57 +0530

    updated local values

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals {
+  resource_location = "West Europe"
+  virtual_network = {
+    name             = "testvnet"
+    address_prefixes = ["10.0.0.0/16"]
+  }
+  subnet_prefix_id = ["10.0.0.0/24", "10.0.1.0/24"]
+}
