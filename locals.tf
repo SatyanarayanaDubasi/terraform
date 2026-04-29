ddd087479198fbfc2a802d196b6ab65b8095ddfd
SatyanarayanaDubasi <satya.dubasi91@gmail.com>
2026-04-29 12:46:58 +0530

    maps values

--- locals.tf
+++ locals.tf
@@ -1,4 +1,9 @@
 locals {
   resource_location = "West Europe"
-  subnet_prefix_id  = ["10.0.0.0/24", "10.0.1.0/24"]
+  virtual_network = {
+    name             = "testvnet"
+    address_prefixes = ["10.0.0.0/16"]
+
+  }
+  subnet_prefix_id = ["10.0.0.0/24", "10.0.1.0/24"]
 }
--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "azurerm_resource_group" "testrg" {
   location = local.resource_location
 }
 resource "azurerm_virtual_network" "testvnet" {
-  name                = "testvnet"
+  name                = local.virtual_network.name
   location            = local.resource_location
   resource_group_name = azurerm_resource_group.testrg.name
   address_space       = ["10.0.0.0/16"]
